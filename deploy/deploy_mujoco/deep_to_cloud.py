3ab50d6cded0f6a082cef4c6d75eb26ba05227d4
iohannes <3140618@163.com>
2025-05-23 13:16:43 +0800

    update

--- deploy/deploy_mujoco/deep_to_cloud.py
+++ deploy/deploy_mujoco/deep_to_cloud.py
@@ -1,4 +1,5 @@
 import open3d as o3d
+import numpy as np
 
 
 class DeepToCloud:
@@ -17,11 +18,21 @@ class DeepToCloud:
             convert_rgb_to_intensity=False
         )
 
+        # Set camera intrinsic parameters with a custom FOV
+        width, height = depth_img.shape
+        fov = 110  # Field of View in degrees
+        fx = fy = (width / 2) / np.tan(np.radians(fov / 2))  # Focal length
+        cx, cy = width / 2, height / 2  # Principal point
+
+        intrinsic = o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx, cy)
+
         # Create point cloud
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
             rgbd,
-            o3d.camera.PinholeCameraIntrinsic(
-                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
-            )
-        )        
+            intrinsic
+        )
+        
+        rotation_matrix_x = pcd.get_rotation_matrix_from_xyz((-90 * np.pi / 180, 90 * np.pi / 180, 0))
+        pcd.rotate(rotation_matrix_x)
+
         return pcd
